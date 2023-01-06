d35a16c9aaf7b0472ab13a85e1666fde6af129dd
N2SuccessAll <camaram9@lsbu.ac.uk>
2023-01-06 01:53:51 +0000

    Display all english alphabet from A to Z.

---
+++ src/simpleBankingSystem/DisplayAllEnglishLetter.java
@@ -0,0 +1,11 @@
+package simpleBankingSystem;
+
+public class DisplayAllEnglishLetter {
+    public static void main(String[] args) {
+        char letter = 'A';
+        while (letter <= 'Z') {
+            System.out.println(letter);
+            letter += 1;
+        }
+    }
+}
